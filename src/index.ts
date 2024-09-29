7dfc3cd334c7c5122a0fb52ffb01578c1e6c6c29
carlosalbertovr <cavelazquezr95@gmail.com>
2024-09-29 20:54:02 +0200

    change api message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
 app.get("/", (_req: Request, res: Response) => {
-	res.json({ msg: "Express on Vercel" });
+	res.json({ msg: "Welcome to Mole Data API😎" });
 });
 
 module.exports = app;
